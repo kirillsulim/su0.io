acd0fec4a1af3383ec908b10198aef5ece7c22e3
Kirill Sulim <kirillsulim@gmail.com>
2018-10-23 22:41:32 +0500

    Up

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import _ from 'lodash';
+import './style.css';
+import duck from './myduck.png';
+import printMe from './print.js';
 
 function component() {
-    let element = document.createElement('div');
-  
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  let element = document.createElement('div');
+  var btn = document.createElement('button');
+
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  var myIcon = new Image();
+  myIcon.src = duck;
+  element.appendChild(myIcon);
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
+  return element;
+}
+
+document.body.appendChild(component());
